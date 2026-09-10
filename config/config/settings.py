a8bf641d1d150a757eb2bd81ace479f1fcbbc14c
Vito <fakerovi4i@gmail.com>
2026-09-10 13:38:37 +0300

    feat: create models, add app in settings and make migrate

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4f54z(a1-dmq#=dqe*bp)b6wqcr&grg18$9hll9^mjsts7*+dh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'crypto.apps.CryptoConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',  # Имя базы данных
+        'USER': 'postgres',  # Имя пользователя БД
+        'PASSWORD': '1234',  # Пароль пользователя
+        'HOST': '127.0.0.1',  # Или 'localhost'
+        'PORT': '5434',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -102,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -115,7 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = []
 
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
--- config/crypto/models.py
+++ config/crypto/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Snapshot(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=100)
+
+
+class CoinPrice(models.Model):
+    snapshot = models.ForeignKey(Snapshot, on_delete=models.CASCADE, related_name='coin_prices')
+    coin_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=100)
+    price = models.FloatField()
+    market_cap = models.FloatField()
+    total_volume = models.FloatField()
+    price_change_percentage_24h = models.FloatField()
